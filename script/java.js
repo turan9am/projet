1a541f51c31db34f6e4af34b9370f65e31109181
turan9am <45905538+turan9am@users.noreply.github.com>
2018-12-16 18:57:38 +0100

    Update java.js

--- script/java.js
+++ script/java.js
@@ -7,13 +7,15 @@ function glisse() {
 		document.getElementById("navbar").style.top = "-57px";
 		document.getElementById("entre").style.top = "-100px";
 		document.getElementById("navbar").style.opacity = "0.4";
+		document.getElementById("gotop_bouton").style.display = "block";
 
 	} else {
 		document.getElementById("navbar").style.position ="";
 		document.getElementById("entre").style.position ="";
 		document.getElementById("navbar").style.top = "0px;";
 		document.getElementById("entre").style.top = "7%";
-		document.getElementById("navbar").style.opacity = "1";}
+		document.getElementById("navbar").style.opacity = "1";
+		document.getElementById("gotop_bouton").style.display = "none";}
 		// document.getElementById("imagetopa").style.opacity = "1";
 		// document.getElementById("imagetop").style.opacity = "1";
 	}
@@ -21,40 +23,46 @@ function glisse() {
 function changecote() {
 	var slide = document.getElementById("slide");
 	slide.style.left="0px";
-	var imagecote = document.getElementById("imagecote");
+	var image_slide_haut = document.getElementById("image_slide_haut");
  	var info= document.getElementById("info");
 	var nom=document.getElementById("nom");
 	var prenom=document.getElementById("prenom"); 
 	var age=document.getElementById("age");
-	var adresse1=document.getElementById("adresse1");
+	var adresse=document.getElementById("adresse");
+	var image_slide_bas =document.getElementById("image_slide_bas");
 
-	imagecote.style.opacity="1";
+	image_slide_haut.style.opacity="1";
 	info.style.opacity="1";
 	nom.style.opacity="1";
 	prenom.style.opacity="1";
 	age.style.opacity="1";
-	adresse1.style.opacity="1";}
+	adresse.style.opacity="1";
+	image_slide_bas.style.opacity="1";}
 
 function normalcote() {
 	var slide = document.getElementById("slide");
 	slide.style.left="-10%";
 
-	var imagecote = document.getElementById("imagecote");
-	var imagecote = document.getElementById("imagecote");
+	var image_slide_haut = document.getElementById("image_slide_haut");
  	var info= document.getElementById("info");
 	var nom=document.getElementById("nom");
 	var prenom=document.getElementById("prenom"); 
 	var age=document.getElementById("age");
-	var adresse1=document.getElementById("adresse1");
+	var adresse=document.getElementById("adresse");
+	var image_slide_bas =document.getElementById("image_slide_bas");
 
-	imagecote.style.opacity="0";
+	image_slide_haut.style.opacity="0";
 	info.style.opacity="0";
 	nom.style.opacity="0";
 	prenom.style.opacity="0";
 	age.style.opacity="0";
-	adresse1.style.opacity="0";}
+	adresse.style.opacity="0";
+	image_slide_bas.style.opacity="0";}
 
 
-          
- // info.style.opacity=nom.style.opacity=prenom.style.opacity=age.style.opacity=adresse1.style.opacity=
 
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 100;
+  document.documentElement.scrollTop = 100;
+}
